57bbabdcc0118ba40c7644a4ff443c2988eea590
Iliana Dulina <ilyana.dulina07@gmail.com>
2026-08-28 14:34:14 +0300

    feat: DiaryEntry model

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class DiaryEntry(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
