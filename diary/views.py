141fb07262a5cea169c224f959a6a284594d27b6
Iliana Dulina <ilyana.dulina07@gmail.com>
2026-08-28 15:01:59 +0300

    feat: DiaryEntry view & template

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import DiaryEntry
+
 
 # Create your views here.
+def diary_home(request):
+    entries = DiaryEntry.objects.all()
+    return render(request, "diary/templates/home.html, {'entries': entries}")
