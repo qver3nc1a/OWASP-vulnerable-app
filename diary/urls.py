239b7b6270583abd7f325c0e65ec9324b70a3625
Iliana Dulina <ilyana.dulina07@gmail.com>
2026-08-31 21:47:11 +0300

    feat: edit entry

--- diary/urls.py
+++ diary/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.diary_home, name="diary_home"),
     path("entry/<int:id>/", views.entry_page, name="entry_page"),
+    path("entry/<int:id>/edit/", views.edit_entry, name="edit_entry"),
 ]
--- diary/views.py
+++ diary/views.py
@@ -19,3 +19,15 @@ def diary_home(request):
 def entry_page(request, id):
     entry = DiaryEntry.objects.get(id=id)
     return render(request, "entry_page.html", {"entry": entry})
+
+
+def edit_entry(request, id):
+    entry = DiaryEntry.objects.get(id=id)
+    if request.method == "GET":
+        form = DiaryEntryForm(instance=entry)
+        return render(request, "edit_entry.html", {"form": form, "entry": entry})
+    elif request.method == "POST":
+        form = DiaryEntryForm(request.POST, instance=entry)
+        if form.is_valid():
+            form.save()
+            return redirect("entry_page", id=id)
